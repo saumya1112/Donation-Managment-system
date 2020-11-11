79edf052c36ea25233c1ab9fb7cf34285e143c91
saumya <saumyagupta093@gmail.com>
2020-11-11 13:07:36 +0530

    modal.css file coorection

--- myaccount.php
+++ myaccount.php
@@ -9,7 +9,7 @@
 
     <!-- ----------link to css file------------- -->
     <link rel="stylesheet" href="myaccount.css">
-    <link rel="stylesheet" href="model.css">
+    <link rel="stylesheet" href="modal.css">
 
     <!-- -----------fontawsm cdn------------- -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
